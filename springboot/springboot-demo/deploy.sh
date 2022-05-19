e51aa40fe406f2e6d5d4262de9c29aad153c051e
daijiong <djflying1988@163.com>
2022-05-19 19:31:46 +0800

    add deploy file

--- springboot/springboot-demo/deploy.sh
+++ springboot/springboot-demo/deploy.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-#set color#
+cd springboot/springboot-demo
 echoRed() { echo $'\e[0;31m'"$1"$'\e[0m'; }
 echoGreen() { echo $'\e[0;32m'"$1"$'\e[0m'; }
 echoYellow() { echo $'\e[0;33m'"$1"$'\e[0m'; }
-
 SERVER_NAME=springboot-demo
-
 REGISTRY_URL=registry.cn-hangzhou.aliyuncs.com
-
 REPOSITORY=$REGISTRY_URL/djflying/$SERVER_NAME
 TAG=kubernetes
-
 echoYellow "=========================>>>>>>>构建本地镜像"
 docker build -t $REPOSITORY:$TAG .
 echoYellow "=========================>>>>>>>向远程镜像仓库中推送镜像"
