fb6915ad8a32e72cf0b00fd61ff2f0c6fadda1fc
daijiong <djflying1988@163.com>
2022-05-24 12:43:23 +0800

    modify k8s deploy files

--- springboot/springboot-demo/k8s/deploy.sh
+++ springboot/springboot-demo/k8s/deploy.sh
@@ -1,3 +1,3 @@
-kubectl delete -f springboot-demo-k8s.yaml
-kubectl apply -f springboot-demo-k8s.yaml
+kubectl delete -f ./springboot-demo-k8s.yaml
+kubectl apply -f ./springboot-demo-k8s.yaml
 echo "k8s deploy success!"
